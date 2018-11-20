e6dffadeb405866865d8992976d63c73a9a979d8
Unknown <mauricebrouwers373@hotmail.com>
2018-11-20 11:46:46 +0100

    added main screen items and detail acctivity class

--- app/src/main/java/com/example/maurice/hueappmarckmaurice/MainActivity.java
+++ app/src/main/java/com/example/maurice/hueappmarckmaurice/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.maurice.hueappmarckmaurice;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button emulatorButton;
+    Button bridgeButton;
+    TextView portTxt;
+    TextView ipTxt;
+    EditText portInputText;
+    EditText ipInputText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        emulatorButton = (Button) findViewById(R.id.emulatorButton);
+        portInputText = (EditText) findViewById(R.id.portInputText);
+        //String portNmr = "";
+                emulatorButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String portNmr = portInputText.getText().toString();
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        DetailActivity.class
+                );
+
+
+
+                startActivity(intent);
+            }
+        });
+
+        bridgeButton = (Button) findViewById(R.id.bridgeButton);
 
 
 
